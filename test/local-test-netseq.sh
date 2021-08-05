dcab51f87a32b62ecabc818a6b75f7e630ad0af7
Robert D Shear <rshear@gmail.com>
2021-08-05 07:32:30 -0400

    fastq->ubam working

--- test/local-test-netseq.sh
+++ test/local-test-netseq.sh
@@ -2,10 +2,6 @@
 cd ~/temp || exit
 conda activate cpa
 
-cromwell run -i ~/Projects/netseq-pipeline/test/inputs.json ~/Projects/netseq-pipeline/netseq-fastq-to-bam.wdl 
-
-#cromwell run -o /Users/robertshear/Projects/netseq-pipeline/macos.conf \
-#    -i ~/Projects/netseq-pipeline/test/inputs.json \
-#     ~/Projects/netseq-pipeline/netseq-fastq-to-bam.wdl 
-
-# java -Dconfig.file=/Users/robertshear/Projects/netseq-pipeline/macos.conf -jar /Users/robertshear/mambaforge/envs/cpa/share/cromwell/cromwell.jar run /Users/robertshear/Projects/netseq-pipeline/netseq-fastq-to-bam.wdl --inputs /Users/robertshear/Projects/netseq-pipeline/test/inputs.json
+cromwell run -i ~/Projects/netseq-pipeline/test/bigtest_inputs.json -t wdl \
+    -o /Users/robertshear/Projects/netseq-pipeline/test/options.json \
+    ~/Projects/netseq-pipeline/netseq-fastq-to-bam.wdl 

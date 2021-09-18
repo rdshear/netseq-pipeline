0d103bab0525b0ecde7f32a2d760386f25107baf
Robert D Shear <rshear@gmail.com>
2021-09-18 13:16:05 -0400

    Scatter/Gather for breakpoints WIP

---
+++ docker_netcpa/build_docker_netcpa.sh
@@ -0,0 +1,3 @@
+cd /Users/robertshear/Projects/netseq-pipeline/docker_netcpa
+docker build . -t rdshear/netcpa
+docker push rdshear/netcpa
---
+++ docker_netseq/build_docker_netseq.sh
@@ -0,0 +1,3 @@
+cd /Users/robertshear/Projects/netseq-pipeline/docker_netseq
+docker build . -t rdshear/netseq
+docker push rdshear/netseq
